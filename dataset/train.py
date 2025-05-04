15996164e7df8b658980bec352261502fb63133d
albrione <albe38488171@gmail.com>
2025-05-05 01:28:06 +0900

    new yolo model train

--- dataset/train.py
+++ dataset/train.py
@@ -1,9 +1,16 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8m.pt")
+def main():
+    model = YOLO("yolov8m.pt")  # 모델 불러오기
+    model.train(
+        data="dataset/data.yaml",
+        epochs=30,
+        imgsz=1024,
+        device="cuda",
+        name="train6"
+    )
 
-model.train(
-    data="dataset/data.yaml",   # ← 같은 디렉토리 기준이므로 이걸로 OK
-    epochs=30,
-    imgsz=640
-)
+if __name__ == "__main__":
+    from multiprocessing import freeze_support
+    freeze_support()  # Windows multiprocessing 문제 해결용
+    main()
